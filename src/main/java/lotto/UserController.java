8e404484fe52563425183e325b7b745e8c03f665
genius00hwan <qnfn120@gmail.com>
2023-07-05 17:26:38 +0900

    feat(UserController): create Constructor and inputWinningNumber Method

--- src/main/java/lotto/UserController.java
+++ src/main/java/lotto/UserController.java
@@ -1,6 +1,35 @@
 package lotto;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class UserController {
 
-}
+	Lotto winningLotto;
+	List<Integer> winningNums;
+	int winPoint;
 
+	UserController() {
+		inputWinningNumbers();
+		winningLotto = new Lotto(winningNums);
+	}
+
+	public void inputWinningNumbers() {
+		winningNums = new ArrayList<Integer>();
+		String inStr = Console.readLine();
+		StringTokenizer tokenizedInStr = new StringTokenizer(inStr, ",");
+		while (tokenizedInStr.hasMoreTokens()) {
+			Integer cur = Integer.parseInt(tokenizedInStr.nextToken());
+			winningNums.add(cur);
+		}
+		for (int a : winningNums) { // debug
+			System.out.println(a);
+		}
+	}
+
+}
