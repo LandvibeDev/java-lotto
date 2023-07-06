7dd70d80c8bcb43206b6d8098c0b4ab12f20055b
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-07-06 18:46:28 +0900

    feat(Lotto) add Lotto class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,2 +1,30 @@
-package lotto;public class Lotto {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public String toString() {
+        ArrayList<Integer> numbers = new ArrayList<>(this.numbers);
+        Collections.sort(numbers);
+        return numbers.toString();
+    }
+
+    // TODO: 추가 기능 구현
 }
