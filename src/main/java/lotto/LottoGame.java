5a49e12b994eebde7526da697a6b3c34c1158e99
박준영 <wnsdud6969@naver.com>
2023-07-06 11:02:26 +0900

    feat(LottoGame): add function to calculate lotto count

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -28,11 +28,16 @@ public class LottoGame implements NumberGame {
 	@Override
 	public void play() {
 		inputPurchaseMoney();
+		calculateLottoCount();
 	}
 
 	private void inputPurchaseMoney() {
 		System.out.println("구입금액을 입력해 주세요.");
 		String input = Console.readLine();
 		purchaseMoney = Integer.parseInt(input);
+	}
+
+	private void calculateLottoCount() {
+		lottoCount = purchaseMoney / 1000;
 	}
 }
