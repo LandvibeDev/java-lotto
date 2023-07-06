d4cb914d9bd2cc4dae5a159ffe3fbe5bd66fd3f9
박준영 <wnsdud6969@naver.com>
2023-07-06 11:28:53 +0900

    feat(LottoGameAnalyzer): add function to calculate rate of return

--- src/main/java/lotto/LottoGameAnalyzer.java
+++ src/main/java/lotto/LottoGameAnalyzer.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class LottoGameAnalyzer {
@@ -14,5 +15,19 @@ public class LottoGameAnalyzer {
 		}
 
 		return score;
+	}
+
+	private double calculateReteOfReturn(int purchaseMoney, Score score) {
+		List<Integer> priceList = Arrays.asList(2000000000, 30000000, 1500000, 50000, 5000);
+		List<Integer> scoreList = score.getScoresAsList();
+		long totalWonMoney = 0;
+
+		for (int iterator = 0; iterator < 5; iterator++) {
+			totalWonMoney += scoreList.get(iterator) * priceList.get(iterator);
+		}
+
+		double rateOfReturn = ((double)totalWonMoney / (double)purchaseMoney) * 100;
+
+		return rateOfReturn;
 	}
 }
