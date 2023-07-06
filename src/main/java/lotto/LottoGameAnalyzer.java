2b555b4380d5c120b2253aad10f054fefbd7d800
박준영 <wnsdud6969@naver.com>
2023-07-06 11:32:11 +0900

    feat(LottoGameAnalyzer): add function to print winning statistics

--- src/main/java/lotto/LottoGameAnalyzer.java
+++ src/main/java/lotto/LottoGameAnalyzer.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 public class LottoGameAnalyzer {
 
+	public void analyze(Lotto winningLotto, int bonusNumber, List<Lotto> lottoList, int purchaseMoney){
+		Score score = calculateScore(winningLotto, bonusNumber, lottoList);
+		double rateOfReturn = calculateReteOfReturn(purchaseMoney, score);
+	}
+
 	private Score calculateScore(Lotto winningLotto, int bonusNumber, List<Lotto> lottoList) {
 		Score score = new Score();
 
@@ -29,5 +34,14 @@ public class LottoGameAnalyzer {
 		double rateOfReturn = ((double)totalWonMoney / (double)purchaseMoney) * 100;
 
 		return rateOfReturn;
+	}
+
+	public void printWinningStatistics(Score score) {
+		System.out.println("\n당첨 통계\n---");
+		System.out.println("3개 일치 (5,000원) - " + score.getWinFifthPlace() + "개");
+		System.out.println("4개 일치 (50,000원) - " + score.getWinFourthPlace() + "개");
+		System.out.println("5개 일치 (1,500,000원) - " + score.getWinThirdPlace() + "개");
+		System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + score.getWinSecondPlace() + "개");
+		System.out.println("6개 일치 (2,000,000,000원) - " + score.getWinFirstPlace() + "개");
 	}
 }
