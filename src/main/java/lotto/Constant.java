c45aa8e82e1688e9514cf4b5b634b223229692ce
박준영 <wnsdud6969@naver.com>
2023-07-06 16:31:30 +0900

    feat(Constant): add enum Rule

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -1,4 +1,20 @@
 package lotto;
 
 public class Constant {
+	public enum Rule {
+		START_NUMBER(1),
+		END_NUMBER(45),
+		NUMBER_SIZE(6);
+
+		private final int number;
+
+		Rule(int number) {
+			this.number = number;
+		}
+
+		public int get() {
+			return number;
+		}
+
+	}
 }
