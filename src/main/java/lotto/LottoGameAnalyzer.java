6416096f0885b3e4216ad1bf50f90c8aabcd3fa7
박준영 <wnsdud6969@naver.com>
2023-07-06 11:23:13 +0900

    feat(LottoGameAnalyzer): add function to calculate score

--- src/main/java/lotto/LottoGameAnalyzer.java
+++ src/main/java/lotto/LottoGameAnalyzer.java
@@ -1,4 +1,18 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoGameAnalyzer {
+
+	private Score calculateScore(Lotto winningLotto, int bonusNumber, List<Lotto> lottoList) {
+		Score score = new Score();
+
+		for (Lotto lotto : lottoList) {
+			int matchCount = lotto.countMatchingNumbers(winningLotto);
+			boolean isBonusNumberMath = lotto.getNumbers().contains(bonusNumber);
+			score.increaseScore(matchCount, isBonusNumberMath);
+		}
+
+		return score;
+	}
 }
