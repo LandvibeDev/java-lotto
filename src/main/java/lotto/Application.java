17ad312d129877c9f1a9bddcf73e86a89763a398
박준영 <wnsdud6969@naver.com>
2023-07-06 16:32:01 +0900

    refactor(Application): update constants using enum Rule

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import static lotto.Constant.Rule.*;
+
 import game.NumberGame;
 
 public class Application {
 	public static void main(String[] args) {
-		Rule lottoGameRule = new Rule(1, 45, 6);
+		Rule lottoGameRule = new Rule(START_NUMBER.get(), END_NUMBER.get(), NUMBER_SIZE.get());
 		NumberGame lottoGame = new LottoGame(lottoGameRule);
 		lottoGame.play();
 	}
