ca4fb2f36e9d2382c118582d05f190b18502dbc5
박준영 <wnsdud6969@naver.com>
2023-07-06 11:22:01 +0900

    feat(Score): add function to increase score according to match count and isBonusNumberMatch

--- src/main/java/lotto/Score.java
+++ src/main/java/lotto/Score.java
@@ -14,4 +14,23 @@ public class Score {
 		this.winFourthPlace = 0;
 		this.winFifthPlace = 0;
 	}
+
+	public void increaseScore(int matchCount, boolean isBonusNumberMatch) {
+		if (matchCount == 6) {
+			winFirstPlace++;
+		}
+		if (matchCount == 5 && isBonusNumberMatch) {
+			winSecondPlace++;
+		}
+		if (matchCount == 5) {
+			winThirdPlace++;
+		}
+		if (matchCount == 4) {
+			winFourthPlace++;
+		}
+		if (matchCount == 3) {
+			winFifthPlace++;
+		}
+	}
+
 }
