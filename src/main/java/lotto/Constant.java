fccb80efd920b7df5b9b3e6c012dc426d017e77a
박준영 <wnsdud6969@naver.com>
2023-07-06 16:56:47 +0900

    feat(Constant): add constant for price of lotto

--- src/main/java/lotto/Constant.java
+++ src/main/java/lotto/Constant.java
@@ -4,7 +4,9 @@ public class Constant {
 	public enum Rule {
 		START_NUMBER(1),
 		END_NUMBER(45),
-		NUMBER_SIZE(6);
+		NUMBER_SIZE(6),
+		LOTTO_PRICE(1000),
+		;
 
 		private final int number;
 
@@ -20,8 +22,7 @@ public class Constant {
 	public enum RequestMessage {
 		REQUEST_INPUT_PURCHASE_MONEY("구입금액을 입력해 주세요."),
 		REQUEST_INPUT_WINNING_LOTTO("\n당첨 번호를 입력해 주세요."),
-		REQUEST_INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.")
-		;
+		REQUEST_INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요.");
 		private final String requestMessage;
 
 		RequestMessage(String message) {
@@ -33,6 +34,5 @@ public class Constant {
 			return requestMessage;
 		}
 	}
-
 
 }
