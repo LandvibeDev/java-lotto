e5681335f2da2f8109100ace698d071f15b643e1
박준영 <wnsdud6969@naver.com>
2023-07-06 11:37:11 +0900

    feat(Application): play lotto game

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import game.NumberGame;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		Rule lottoGameRule = new Rule(1, 45, 6);
+		NumberGame lottoGame = new LottoGame(lottoGameRule);
+		lottoGame.play();
+	}
 }
+
