cacdc0067e5785efec8a26684369a66dfbfd2160
박준영 <wnsdud6969@naver.com>
2023-07-06 10:56:51 +0900

    feat(Score): add class Score

---
+++ src/main/java/lotto/Score.java
@@ -0,0 +1,17 @@
+package lotto;
+
+public class Score {
+	int winFirstPlace;
+	int winSecondPlace;
+	int winThirdPlace;
+	int winFourthPlace;
+	int winFifthPlace;
+
+	public Score() {
+		this.winFirstPlace = 0;
+		this.winSecondPlace = 0;
+		this.winThirdPlace = 0;
+		this.winFourthPlace = 0;
+		this.winFifthPlace = 0;
+	}
+}
