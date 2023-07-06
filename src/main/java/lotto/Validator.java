f797a0bed629f5bbf3fa63e350f6af5a6eb6d22f
박준영 <wnsdud6969@naver.com>
2023-07-06 16:03:46 +0900

    feat(validator): add function to validate winning lotto

--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,5 +1,10 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Validator {
 	public void validatePurchaseMoney(String input) {
 		for (char digit : input.toCharArray()) {
@@ -18,4 +23,26 @@ public class Validator {
 			throw new IllegalArgumentException("[ERROR] 로또 가격 단위로 로또를 구매할 수 없습니다.");
 		}
 	}
+
+	public void validateWinningLotto(String input) {
+		List<Integer> winningNumbers = new ArrayList<>();
+		try {
+			winningNumbers = Arrays.stream(input.split(","))
+				.map(Integer::parseInt)
+				.collect(Collectors.toList());
+		} catch (Exception e) {
+			throw new IllegalArgumentException("[ERROR] 올바른 형식으로 입력해 주세요.");
+		}
+
+		if (winningNumbers.size() != 6) {
+			throw new IllegalArgumentException("[ERROR] 올바른 개수의 번호를 입력해 주세요");
+		}
+
+		for (int number : winningNumbers) {
+			if (number < 1 || number > 45) {
+				throw new IllegalArgumentException("[ERROR] 올바른 범위의 번호를 입력해 주세요");
+			}
+		}
+	}
+
 }
