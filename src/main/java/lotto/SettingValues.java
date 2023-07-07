52e0c0b657dcd94f371f5bf4e0264d5dc020b087
genius00hwan <qnfn120@gmail.com>
2023-07-07 16:47:21 +0900

    add(SettingValues): add values and constructor for SettingValues

--- src/main/java/lotto/SettingValues.java
+++ src/main/java/lotto/SettingValues.java
@@ -1,5 +1,26 @@
 package lotto;
 
 public enum SettingValues {
+	MIN_OF_RANGE(1),
+	MAX_OF_RANGE(45),
+	NUMBER_SIZE(6),
+	PRICE_OF_LOTTO(1000),
+	WINNING_RANGE(5),
+	REWARD_OF_FIRST(2000000000),
+	REWARD_OF_SECOND(30000000),
+	REWARD_OF_THIRD(1500000),
+	REWARD_OF_FOURTH(50000),
+	REWARD_OF_FIFTH(5000),
+	FAIL(0),
+	ZERO(0);
 
+	private final int value;
+
+	SettingValues(int value) {
+		this.value = value;
+	}
+
+	public int get() {
+		return value;
+	}
 }
