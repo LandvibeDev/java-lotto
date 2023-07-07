7a90b250885dcdcef00daaa54f97dc1dbd9808b5
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-07-07 19:46:19 +0900

    feat(LottoList): add LottoList class

--- src/main/java/lotto/LottoList.java
+++ src/main/java/lotto/LottoList.java
@@ -1,2 +1,22 @@
-package lotto;public class LottoList {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class LottoList {
+    Lotto[] lottos;
+    int coin;
+    LottoList(int coin){
+        this.coin = coin;
+        this.lottos = new Lotto[coin];
+    }
+
+    public void setLottoList() {
+        for (int i = 0; i < coin; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            lottos[i] = new Lotto(numbers);
+            System.out.println(lottos[i].toString());
+        }
+    }
 }
