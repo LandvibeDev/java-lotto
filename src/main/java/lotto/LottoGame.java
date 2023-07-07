a296f4bfa496721d440f3f9d0b574fcecc39219b
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-07-07 19:47:14 +0900

    fear(LottoGame) add LottoGame class

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,2 +1,25 @@
-package lotto;public class LottoGame {
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoGame {
+    ExceptionController exceptionController = new ExceptionController();
+    public void run(){
+        int amount = getPurchaseAmount();
+        LottoList lottoList = new LottoList(amount/1000);
+        lottoList.setLottoList();
+    }
+
+    public int getPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요.");
+
+        String amount = Console.readLine();
+        exceptionController.noIntegerValueExeption(amount);
+        exceptionController.noValidAmount(amount);
+
+        return Integer.parseInt(amount);
+    }
+
+
+
 }
