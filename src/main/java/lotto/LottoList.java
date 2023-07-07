335b415cd40062639c8dd13df8b85afaa6ffcf37
sheepysnoopy <qkffldlfdjsk12@naver.com>
2023-07-07 21:10:10 +0900

    styl(Lottolist): refunction setLottoList()

--- src/main/java/lotto/LottoList.java
+++ src/main/java/lotto/LottoList.java
@@ -11,10 +11,10 @@ public class LottoList {
         System.out.println(coin + "개를 구매했습니다.");
 
         this.lottos = new Lotto[coin];
-        printLottoList(coin);
+        setLottoList(coin);
     }
 
-    public void printLottoList(int coin) {
+    public void setLottoList(int coin) {
         for (int i = 0; i < coin; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             lottos[i] = new Lotto(numbers);
