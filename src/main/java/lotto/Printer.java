35c3a22ead92dbf151c7a3ce06b4f37790666998
genius00hwan <qnfn120@gmail.com>
2023-07-09 00:22:03 +0900

    add(printer) : add methods for input messages and divide the printResult func

--- src/main/java/lotto/Printer.java
+++ src/main/java/lotto/Printer.java
@@ -1,22 +1,48 @@
 package lotto;
 
-public class Printer {
-	public void printResult(int[] rankings, double totalReward, double purchaseAmount) {
-		double profitRatio = 0;
-		System.out.println("당첨 통계\n" + "---");
-		System.out.print("3개 일치 (5,000원) - ");
-		System.out.println(rankings[5] + "개");
-		System.out.print("4개 일치 (50,000원) - ");
-		System.out.println(rankings[4] + "개");
-		System.out.print("5개 일치 (1,500,000원) - ");
-		System.out.println(rankings[3] + "개");
-		System.out.print("5개 일치, 보너스 볼 일치 (30,000,000원) - ");
-		System.out.println(rankings[2] + "개");
-		System.out.print("6개 일치 (2,000,000,000원) - ");
-		System.out.println(rankings[1] + "개");
-		profitRatio = (totalReward / purchaseAmount) * 100.0;
-		System.out.printf("총 수익률은 %.1f", profitRatio);
-		System.out.println("%입니다.");
+import static lotto.Messages.*;
+import static lotto.SettingValues.*;
+import static lotto.SharingVariables.*;
 
+public class Printer {
+	public void printInputPurchaseAmountMessage() {
+		System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE.get());
+	}
+
+	public void printNumberOfPurchaseMessage(int number) {
+		System.out.println(number + NUMBER_OF_PURCHASE_MESSAGE.get());
+	}
+
+	public void printInputWinningNumMessage() {
+		System.out.println(INPUT_WINNING_NUMS_MESSAGE.get());
+	}
+
+	public void printInputBonusNumMessage() {
+		System.out.println(INPUT_BONUS_NUM_MESSAGE.get());
+	}
+
+	public void printResult(double totalReward, double purchaseAmount) {
+		printReceiptTitle();
+		for (int i = FIRST.get(); i <= FIFTH.get(); i++) {
+			printRankingResult(i);
+		}
+		printProfitRatio(totalReward, purchaseAmount);
+	}
+
+	public void printRankingResult(int i) {
+		System.out.printf(formats[i], points[i], rewadsStr[i], numOfRanking[i]);
+		System.out.println();
+	}
+
+	public void printReceiptTitle() {
+		System.out.println(RECEIPT_TITLE.get());
+	}
+
+	public void printProfitRatio(double totalReward, double purchaseAmount) {
+		double profitRatio;
+		profitRatio = (totalReward / purchaseAmount) * PERCENTAGE;
+		System.out.printf(PROFIT_RATIO_MESSAGE_FORMAT1.get(), profitRatio);
+		System.out.print(PROFIT_RATIO_MESSAGE_FORMAT2.get());
+		System.out.println();
 	}
 }
