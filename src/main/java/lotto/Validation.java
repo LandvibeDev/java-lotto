1b354b9b9aede16572abd7a61dec9ed25c6d7ef0
HyeongSeok <gudtjr051911@gmail.com>
2023-07-08 21:42:25 +0900

    feat(Validation): it works for validation of input

---
+++ src/main/java/lotto/Validation.java
@@ -0,0 +1,66 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Validation {
+
+    public void validateMoney(String money) {
+        moneyCharacterCheck(money);
+        checkAmountOfMoney(money);
+    }
+
+    public void validateWinningNumbers(String mainNumbers, String bonusNumber) {
+        mainNumberCharacterCheck(mainNumbers);
+        bonusNumberCharacterCheck(bonusNumber);
+        winningNumberDuplicationCheck(mainNumbers, bonusNumber);
+    }
+
+    private void mainNumberCharacterCheck(String mainNumbers) {
+        String[] mainNumberList = mainNumbers.split(",");
+        for (String mainNumber : mainNumberList) {
+            if(mainNumber == "0") {
+                throw new IllegalArgumentException(ErrorMessage.WRONG_CHARACTER.message);
+            }
+            for (Character character : mainNumber.toCharArray()) {
+                if (!Character.isDigit(character)) {
+                    throw new IllegalArgumentException(ErrorMessage.WRONG_CHARACTER.message);
+                }
+            }
+        }
+    }
+
+    private void bonusNumberCharacterCheck(String bonusNumber) {
+        for (Character character : bonusNumber.toCharArray()) {
+            if (!Character.isDigit(character)) {
+                throw new IllegalArgumentException(ErrorMessage.WRONG_CHARACTER.message);
+            }
+        }
+    }
+
+    private void winningNumberDuplicationCheck(String inputMainNumbers, String inputBonusNumber) {
+        Set<Integer> set = new HashSet<>();
+        for (String number : inputMainNumbers.split(",")) {
+            set.add(Integer.parseInt(number));
+        }
+        set.add(Integer.parseInt(inputBonusNumber));
+        if (set.size() != Rule.COUNT + 1) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATION.message);
+        }
+    }
+
+    private void moneyCharacterCheck(String money) {
+        for (char character : money.toCharArray()) {
+            if (!Character.isDigit(character)) {
+                throw new IllegalArgumentException(ErrorMessage.WRONG_CHARACTER.message);
+            }
+        }
+    }
+
+    private void checkAmountOfMoney(String money) {
+        if(Integer.parseInt(money) % 1000 != 0 || Integer.parseInt(money) == 0) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_MONEY.message);
+        }
+    }
+}
