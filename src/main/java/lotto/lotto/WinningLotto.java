b9fc5fe206b6f140cbb469c017ce15fd65eba08e
gimdongha <d11210920@gmail.com>
2023-07-08 11:15:41 +0900

    feat(WinningLotto):add WinningLotto class to save WinLotto

---
+++ src/main/java/lotto/lotto/WinningLotto.java
@@ -0,0 +1,18 @@
+package lotto.lotto;
+
+public class WinningLotto {
+    private final Lotto lotto;
+    private final int bonus;
+    public WinningLotto(Lotto lotto, int bonus){
+        this.lotto = lotto;
+        this.bonus = bonus;
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+}
