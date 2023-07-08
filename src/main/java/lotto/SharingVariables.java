070f29e3a422cd89dc2bd8a1481bd6110b2bebf8
genius00hwan <qnfn120@gmail.com>
2023-07-08 21:51:53 +0900

    feat(SharingVariables): create Sharing Values

---
+++ src/main/java/lotto/SharingVariables.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import static lotto.SettingValues.*;
+
+public class SharingVariables {
+	public static final int[] rewards = new int[] {FAIL.get(), REWARD_OF_FIRST.get(), REWARD_OF_SECOND.get(),
+		REWARD_OF_THIRD.get(), REWARD_OF_FOURTH.get(), REWARD_OF_FIFTH.get()};
+	public static int[] rankings = new int[6];
+
+}
