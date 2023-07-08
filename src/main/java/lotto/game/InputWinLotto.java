a0ab29c0d38b7dbd93b917ed90bddc0450fb3c02
gimdongha <d11210920@gmail.com>
2023-07-08 15:05:26 +0900

    refactor(InputWinLotto):refactor logic to check exception

--- src/main/java/lotto/game/InputWinLotto.java
+++ src/main/java/lotto/game/InputWinLotto.java
@@ -1,10 +1,12 @@
 package lotto.game;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.checkexception.CheckCharException;
 import lotto.lotto.Lotto;
 import lotto.lotto.WinningLotto;
 import lotto.checkexception.CheckException;
 import lotto.checkexception.CheckExceptionImpl;
+import lotto.numbers.RandomRange;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,17 +20,24 @@ public class InputWinLotto {
         String input = Console.readLine().toString();
         String[] words = input.split(",");
         List<Integer> list = new ArrayList<>();
+        CheckException checkException = new CheckExceptionImpl();
+        CheckCharException checkCharException = new CheckCharException();
         for (String word : words) {
-            CheckException checkException = new CheckExceptionImpl();
+            checkCharException.checkCharException(word);
             int check = Integer.parseInt(word);
             checkException.checkException(check);
+            if(list.contains(check)) throw new IllegalArgumentException("[ERROR]로또 번호는 중복될 수 없습니다.");
             list.add(check);
         }
+        if(list.size() != RandomRange.SIZE.getValue()) throw new IllegalArgumentException("[ERROR]숫자 6개를 입력해주세요.");
 
         lotto = new Lotto(list);
         System.out.println("보너스 번호를 입력해 주세요.");
         String inputBonus = Console.readLine();
+        checkCharException.checkCharException(inputBonus);
         int bonus = Integer.parseInt(inputBonus);
+        checkException.checkException(bonus);
+        if(list.contains(bonus)) throw new IllegalArgumentException("[ERROR]로또 번호는 중복될 수 없습니다.");
         winningLotto = new WinningLotto(lotto,bonus);
     }
 
