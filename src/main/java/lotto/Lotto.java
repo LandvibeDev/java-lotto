a4c0ede051a649316a4e00889536c2587097f56e
임주민 <limj1213@gmail.com>
2023-07-10 03:30:41 +0900

    feat(Lotto): add a Lotto class

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,27 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        Collections.sort(numbers);
+        return numbers;
+    }
+}
