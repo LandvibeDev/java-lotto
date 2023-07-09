fef20a6af469c8adbcb7dd6b8e9433ebe9d36b4e
HyeongSeok <gudtjr051911@gmail.com>
2023-07-09 13:46:13 +0900

    refactor(RandomNumberGenerator): edit function

--- src/main/java/lotto/RandomNumberGenerator.java
+++ src/main/java/lotto/RandomNumberGenerator.java
@@ -2,25 +2,14 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 import static lotto.Rule.*;
 
 public class RandomNumberGenerator {
 
-    public ArrayList<Integer> generateRandomNumbers() {
-        Set<Integer> numberSet = new HashSet<>();
-        ArrayList<Integer> randomNumbers = new ArrayList<>();
-        while (numberSet.size() < COUNT) {
-            int randomNumber = Randoms.pickNumberInRange(START, END);
-            if (numberSet.contains(randomNumber)) {
-                continue;
-            }
-            randomNumbers.add(randomNumber);
-            numberSet.add(randomNumber);
-        }
+    public List<Integer> generateRandomNumbers() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(START, END, COUNT);
         return randomNumbers;
     }
 }
