cc4713830073df39eb5e5d643e092ae7b1fb51d4
임주민 <limj1213@gmail.com>
2023-07-10 03:37:44 +0900

    feat(LottoManager): add two functions that issue lottery

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -3,6 +3,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoManager {
     private MyLottoData myLotto;
     private WinnerLottoData winnerLotto;
@@ -20,6 +23,26 @@ public class LottoManager {
         validatePurchaseMoney(input);
         int money = Integer.parseInt(input);
         myLotto.money = money;
+    }
+
+    private void issueLottoMultiSet() {
+        myLotto.quantity = myLotto.money / Number.Rule.PRICE.toNumber();
+        while(myLotto.lottoSet.size() < myLotto.quantity) {
+            Lotto lottoOneSet = issueLottoOneSet();
+            myLotto.lottoSet.add(lottoOneSet);
+        }
+    }
+
+    private Lotto issueLottoOneSet() {
+        List<Integer> lottoOneList = new ArrayList<>();
+        while(lottoOneList.size() < Number.Rule.SIZE.toNumber()) {
+            int newNumber = Randoms.pickNumberInRange(Number.Rule.MIN.toNumber(), Number.Rule.MAX.toNumber());
+            if(lottoOneList.contains(newNumber)) {
+                continue;
+            }
+            lottoOneList.add(newNumber);
+        }
+        return new Lotto(lottoOneList);
     }
 
     // TODO: - 유효성 체크
